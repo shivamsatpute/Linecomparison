d3f0ff539074680b41aff691f1dedea02313fda0
shivam satpute <shivam897satpute@gmail.com>
2021-05-06 20:14:55 +0530

    [shivam]Add: check the lines are equal or not

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,26 +1,43 @@
 ﻿using System;
 
 namespace PracticeProblem
-{   /*  As a fan of geometry, I want to
-        model a line based on a point
-        consisting of (x, y) co-ordinates using the Cartesian system, So that I can calculate its length.
-        - A Length as 2 Points (x1, y1) and (x2, y2) 
-        - Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2 - y1) ^ 2)
-    */
+{  
     class LineComparisonProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter x1 Value:- ");             // Take x1 input value
-            int x1 = Convert.ToInt32(Console.ReadLine());       //Convert x1 value integer 
-            Console.WriteLine("Enter x2 Value:- ");              // Take x2 input value
-            int x2 = Convert.ToInt32(Console.ReadLine());         //Convert x2 value integer 
-            Console.WriteLine("Enter y1 Value:- ");                // Take y1 input value
-            int y1 = Convert.ToInt32(Console.ReadLine());            //Convert y1 value integer
-            Console.WriteLine("Enter y2 Value:- ");                 // Take y2 input value
-            int y2 = Convert.ToInt32(Console.ReadLine());   //Convert y2 value integer
-            double LengthofLine = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);  // Calculate LenghthofLine using Math.Sqrt formula
-            Console.WriteLine("Length of (x, y) co-ordinates -  " + LengthofLine);  // Display Length
+            Console.WriteLine("Enter x1 Value:- ");             
+            int x1 = Convert.ToInt32(Console.ReadLine());       
+            Console.WriteLine("Enter x2 Value:- ");              
+            int x2 = Convert.ToInt32(Console.ReadLine());          
+            Console.WriteLine("Enter y1 Value:- ");               
+            int y1 = Convert.ToInt32(Console.ReadLine());            
+            Console.WriteLine("Enter y2 Value:- ");                 
+            int y2 = Convert.ToInt32(Console.ReadLine());       
+
+
+            Console.WriteLine("Enter j1 Value:- ");           
+            int j1 = Convert.ToInt32(Console.ReadLine());      
+            Console.WriteLine("Enter j2 Value:- ");             
+            int j2 = Convert.ToInt32(Console.ReadLine());         
+            Console.WriteLine("Enter k1 Value:- ");                
+            int k1 = Convert.ToInt32(Console.ReadLine());           
+            Console.WriteLine("Enter k2 Value:- ");                 
+            int k2 = Convert.ToInt32(Console.ReadLine());     
+
+            double LengthofLine1 = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);  
+            double LengthofLine2 = Math.Sqrt((j2 - j1) ^ 2 + (k2 - k1) ^ 2);  
+            Console.WriteLine("Length of (x, y) co-ordinates -  " + LengthofLine1);  
+            Console.WriteLine("Length of (j, k) co-ordinates -  " + LengthofLine2); 
+
+            if (LengthofLine1.Equals(LengthofLine2))            
+            {
+                Console.WriteLine("Two lines are equal ");
+            }
+            else
+            {
+                Console.WriteLine("Two lines are Not equal");
+            }
         }
     }
 }
